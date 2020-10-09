c0c885d93ccf176dd8f04cec3c980dab95397486
Peter Wood <peter.wood@anontechnology.com>
2020-10-09 15:36:41 -0400

    initial commit of vault interface

---
+++ src/main/java/io/vizivault/ViziVault.java
@@ -0,0 +1,64 @@
+package main.java.io.vizivault;
+
+public class ViziVault {
+
+  private String baseUrl;
+  private String clientId;
+  private String apiKey;
+  private String encryptionKey;
+  private String decryptionKey;
+
+  public ViziVault withBaseURL(String url) {
+    this.baseUrl = url;
+    return this;
+  }
+
+  public ViziVault withClientId(String clientId) {
+    this.clientId = clientId;
+    return this;
+  }
+
+  public ViziVault withApiKey(String apiKey) {
+    this.apiKey = apiKey;
+    return this;
+  }
+
+  public ViziVault withEncryptionKey(String encryptionKey) {
+    this.encryptionKey = encryptionKey;
+    return this;
+  }
+
+  public ViziVault withDecryptionKey(String decryptionKey) {
+    this.decryptionKey = decryptionKey;
+    return this;
+  }
+
+  public Entity findByEntity(String entityId) {
+    //...
+  }
+
+  public void save(Entity entity) {
+    //...
+  }
+
+  public void purge(Entity entity) {
+    // ...
+  }
+
+  public void remove(Entity entity, String attributeKey) {
+    // ...
+  }
+
+  public void storeAttribute(Attribute attribute) {
+    // ...
+  }
+
+  public void storeTag(Tag tag) {
+    // ...
+  }
+
+  public void storeRegulation(Regulation regulation) {
+    // ...
+  }
+
+}
